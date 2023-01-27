8651bf8668663161049c086feb70f3e9cb8e08ea
Shubham20006 <shubhamchaudhari1506@gmail.com>
2023-01-27 21:09:16 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndStateCensusAnalyser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Indian State Census Analyser Program");
+        }
+    }
+}
